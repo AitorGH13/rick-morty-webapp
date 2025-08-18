1dbe6eaf667d0886c0545f00365bbdb57d4750ea
AitorGH13 <148903609+AitorGH13@users.noreply.github.com>
2025-08-18 20:36:51 +0200

    Code refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 import { registerRootComponent } from 'expo';
 
 if (Platform.OS === 'web') {
-  import('./App').then(({ default: App }) => {
+  import('./src/App').then(({ default: App }) => {
     import('react-dom/client').then(({ createRoot }) => {
       const container = document.getElementById('root');
       const root = createRoot(container);
@@ -10,7 +10,7 @@ if (Platform.OS === 'web') {
     });
   });
 } else {
-  import('./App').then(({ default: App }) => {
+  import('./src/App').then(({ default: App }) => {
     registerRootComponent(App);
   });
 }
