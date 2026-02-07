a47a859c3e15aad1fb8e793793417b1f7bffb1e6
Aitor Arrarás <148903609+AitorGH13@users.noreply.github.com>
2026-02-07 07:33:34 +0100

    refactor and deploy (#10)
    * project refactor and deploy

---
+++ src/App.web.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import MiddelBar from './components/layout/MiddleBar';
+import Sidebar from './components/layout/Sidebar';
+import Navbar from './components/layout/Navbar';
+
+import Home from './screens/Home';
+import SobreNosotros from './screens/SobreNosotros';
+import PreguntasFrecuentes from './screens/PreguntasFrecuentes';
+
+import ScreenLugares from './screens/ScreenLugares';
+import ScreenPersonajes from './screens/ScreenPersonajes';
+import Episodios from './screens/ScreenEpisodios';
+
+import './styles/webController.css';
+
+function App() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const toggleSidebar = () => setOpenSidebar((o) => !o);
+
+  return (
+    <Router>
+      <div style={{ minHeight: '100vh' }}>
+        <Navbar />
+
+        <div className="header">
+          <MiddelBar toggleSidebar={toggleSidebar} />
+          <Sidebar open={openSidebar} toggleSidebar={toggleSidebar} />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<SobreNosotros />} />
+            <Route path="/faq" element={<PreguntasFrecuentes />} />
+            <Route path="/lugares" element={<ScreenLugares />} />
+            <Route path="/personajes" element={<ScreenPersonajes />} />
+            <Route path="/episodios" element={<Episodios />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
